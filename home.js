35c5d963233feb246bf8ee06cc91350e45726349
Happy Baskaran <behappybas@gmail.com>
2018-05-06 13:22:57 +0530

    home.js changes

--- home.js
+++ home.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+var path = require('path');
 
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/Views/partials')
 app.set('view engine','hbs');
 app.use(express.static(__dirname+'/public'));
-
-app.use((req,res,next)=>{
-  var now = new Date().toString();
-  let log = `${now} : ${req.method} ${req.url}`
-  console.log(log);
-  fs.appendFile('server.log',log+'\n',(err)=>{
-    if(err){
-      console.log('Uanble to append to server.log.');
-    }
-  });
-  next();
-});
-
-hbs.registerHelper('getCurrentYear',()=>{
-  return new Date().getFullYear()
-})
-
-hbs.registerHelper('screamIt',(text)=>{
-  return text.toUpperCase();
-})
+app.use(express.static(__dirname+'/public/assets'));
+app.use(express.static(__dirname+'/public/images'));
 
 app.get('/',(req,res)=>{
   res.render('index.hbs');
@@ -43,24 +26,6 @@ app.get('/listenit',(req,res)=>{
   res.render('listenit.hbs',{
     Heading:"Ease your Reading",
     BHeader:"Make it Simple"
-  });
-});
-
-app.get('/project',(req,res)=>{
-  res.render('project.hbs',{
-    header:'to Project Page',
-    Heading:'Here you are in my Project Page'
-  });
-});
-
-app.get('/contactus',(req,res)=>{
-  res.send('Contact us Page');
-})
-
-app.get('/bad',(req,res)=>{
-  res.send({
-    error:"unable to get the required page",
-    handler:"Try to refresh the page"
   });
 });
 
